1be9514e5930b0cea186b30369b0d746a2aab3be
Michael <phamm89@gmail.com>
2019-06-04 13:34:18 -0400

    Palindrome Test

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,19 @@
+# User enters string
 palindrometest = str(input("Enter a word, phrase, sentence, or multiple sentences: "))
 
+# Letters of the alphabet
 all_letters = "abcdefghijklmnopqrstuvwxyz"
+
+# Blank Array
 input_letters = []
 
+# Search user's string input and enter all letters into new array
 for letter in palindrometest.lower():
     if letter in all_letters:
         input_letters.append(letter)
 print(input_letters)
 
+#Compare the string forwards with the string backwards to determine if palindrome
 if input_letters[:] == input_letters[::-1]:
     print("The input " + str(palindrometest) + " is a palindrome.")
 else:
