39055cc29b3e6c3a2c12ad4c0dace3b517ea120f
Michael <phamm89@gmail.com>
2019-06-04 13:03:28 -0400

    Palindrome Test

--- palindrome.py
+++ palindrome.py
@@ -6,7 +6,7 @@ input_letters = []
 for letter in palindrometest.lower():
     if letter in all_letters:
         input_letters.append(letter)
-    # print(input_letters)
+print(input_letters)
 
 if input_letters[:] == input_letters[::-1]:
     print("The input " + str(palindrometest) + " is a palindrome.")
