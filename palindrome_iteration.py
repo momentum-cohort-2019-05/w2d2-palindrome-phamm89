f949fae899f33864e1fb5767330feebccecb2508
Michael <phamm89@gmail.com>
2019-06-04 20:45:29 -0400

    Palindrome Test

--- palindrome_iteration.py
+++ palindrome_iteration.py
@@ -16,14 +16,18 @@ print(input_letters)
 #Iterative function to determine if string is palindrome
 def palindrome_iteration(input_letters):
     """Compare letters in input_letters to determine if palindrome."""
+    # Define a counter for the first letter and endcounter for the last letter
     counter = 0
     endcounter = len(input_letters) - 1
+    # If string has no entry or has one letter, then it is a palindrome
     if len(input_letters) == 1 or len(input_letters) == 0:
         palindrome = "The input " + str(palindrometest) + " is a palindrome."
         return print(str(palindrome))
+    # If first and last letter are not equal, then it is not a palindrome
     elif input_letters[0] != input_letters[-1]:
         not_palindrome = "The input " + str(palindrometest) + " is not a palindrome."
         return print(str(not_palindrome))
+    # Iterative part of function to compare corresponding positions of string using counting variables
     elif input_letters[counter] == input_letters[endcounter]:
         while counter <= len(input_letters):
             if input_letters[counter] == input_letters[endcounter]:
--- palindrome_recursion.py
+++ palindrome_recursion.py
@@ -16,12 +16,15 @@ print(input_letters)
 #Recursive function to determine if string is palindrome
 def palindrome_recursion(input_letters):
     """Compare letters in input_letters to determine if palindrome."""
+    # If string has no entry or has one letter, then it is a palindrome
     if len(input_letters) == 1 or len(input_letters) == 0:
         palindrome = "The input " + str(palindrometest) + " is a palindrome."
         return print(str(palindrome))
+    # If the first and last letters are not equal, then it is not a palindrome
     elif input_letters[0] != input_letters[-1]:
         not_palindrome = "The input " + str(palindrometest) + " is not a palindrome."
         return print(str(not_palindrome))
+    # Recursive portion to compare corresponding positions of string  
     elif input_letters[0] == input_letters[-1]:
         return palindrome_recursion(input_letters[1:-1])
     else:
